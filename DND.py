cec046bb4c09a3550626b3f3597e4b45415ff52c
ugurcankuzu <58211421+ugurcankuzu@users.noreply.github.com>
2020-03-05 05:18:50 +0300

    Update DND.py

--- DND.py
+++ DND.py
@@ -9,42 +9,42 @@ D&D DICE ROLLER by Uğurcan
 
 
 while True:
-    a = int(input("Lütfen Kaçlık Zar Atacağınızı Giriniz:"))
+    a = int(input("Please Choose Your Dice:"))
 
     if a == 2:
         x = [1,2]
         random.shuffle(x)
         y = random.choice(x)
-        print("Zarınız :",y)
+        print("Dice :",y)
     elif a == 4:
         x = [1,2,3,4]
         random.shuffle(x)
         y = random.choice(x)
-        print("Zarınız: ",y)
+        print("Dice: ",y)
 
     elif a == 6:
         x = [1,2,3,4,5,6]
         random.shuffle(x)
         y = random.choice(x)
-        print("Zarınız: ",y)
+        print("Dice: ",y)
     elif a == 8:
         x = range(1,9)
         y = random.choice(x)
-        print("Zarınız: ",y)
+        print("Dice: ",y)
     elif a == 10:
         x = range(1,11)
         y = random.choice(x)
-        print("Zarınız: ",y)
+        print("Dice: ",y)
     elif a == 12:
         x = range(1,13)
         y = random.choice(x)
-        print("Zarınız: ",y)
+        print("Dice: ",y)
     elif a == 20:
         x = range(1,21)
         y = random.choice(x)
-        print("Zarınız: ",y)
+        print("Dice: ",y)
     elif a == 0:
-        print("Bugünlük bu kadar oyun yeter galiba :)")
+        print("Hey DM! That's Enough For Today :)")
         break
     else:
-        print("Lütfen geçerli bir zar gir DM :)")
+        print("That's Wrong number. We haven't this dice yet DM :)")
